7744a8e1e05373bb528931116c2edac032ed1004
Steven Scott <stevescott2653@gmail.com>
2019-10-01 17:56:42 -0400

    Add files via upload

---
+++ Finally_Block/Finally_Block/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finally_Block
+{
+    using System.Xml;
+
+    public class ExEample
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int x = a / b;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+                {
+                    Console.WriteLine("Finally block is executed");
+                }
+
+            Console.WriteLine("Rest of the code");
+        }
+    }
+}
